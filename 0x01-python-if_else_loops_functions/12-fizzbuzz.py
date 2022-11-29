5d341a701d55204461ebae1d075264889a2eb9ba
kefaslungu <jameskefaslungu@gmail.com>
2022-11-29 21:33:14 +0100

    final push for the C shell project!

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i%3 ==0 and i %5==0:
+        if i%3 == 0 and i %5 == 0:
             print('Fizz Buzz ')
         elif i % 3 == 0:
             print('Fizz ')
-        elif i %5 ==0:
+        elif i %5 == 0:
             print('Buzz ')
         else:
-            print(i, +' ')
+            print(f"{i} ")

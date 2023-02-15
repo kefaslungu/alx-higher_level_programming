eb332f99e7cc6f0285301f8afab70346ce2e048b
kefaslungu <jameskefaslungu@gmail.com>
2023-02-15 19:57:39 +0100

    0x0E-SQL_more_queries

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- write an sql  script that Lists all cities of CA in the database hbtn_0d_usa.
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;

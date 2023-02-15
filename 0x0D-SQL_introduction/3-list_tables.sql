c4e198cbef9845cfda9027ec2e325c6a0b1e13a6
kefaslungu <jameskefaslungu@gmail.com>
2023-02-15 15:31:22 +0100

    update  to mysql!

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,5 +1,2 @@
 -- get list of tables
-USE information_schema;
-SELECT TABLE_NAME
-FROM TABLES
-WHERE TABLE_SCHEMA = 'mysql';
+USE information_schema; SELECT TABLE_NAME FROM TABLES WHERE TABLE_SCHEMA = 'mysql';

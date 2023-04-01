57b73fbdf0782df44d0637a56940fe23726ddbcf
kefaslungu <jameskefaslungu@gmail.com>
2023-04-01 03:09:51 +0100

    updates

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""A script that takes in a URL using sys.argv, Sends a request to the URL,
-And displays the value of the X-Request-Id variable found in the header of the response.
+"""Displays the X-Request-Id header variable of a request to a given URL.
+Usage: ./1-hbtn_header.py <URL>
 """
-
-import urllib.request
 import sys
+import urllib.request
 
-url = sys.argv[1]
 
-with urllib.request.urlopen(url) as response:
-    headers = response.info()
-    x_request_id = headers.get('X-Request-Id')
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-print(x_request_id)
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+"""Sends a request to a given URL and displays the response body.
+Usage: ./7-error_code.py <URL>
+  - Handles HTTP errors.
 """
-Python Script that takes in a URL, sends a request to the URL
-& displays the body of the response
-"""
+import sys
 import requests
-from sys import argv
 
-if __name__ == '__main__':
-    r = requests.get(argv[1])
-    status = r.status_code
-    print(r.text) if status < 400 else print(
-        "Error code: {}".format(r.status_code))
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
